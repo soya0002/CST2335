cf277c042574cd6354e147eb36ba19f30820dfab
soya0002 <badal_bad123>
2018-02-02 16:45:24 -0500

    Added SharedPreference Lab3

--- app/src/main/java/androidlabs/com/androidlabs/LoginActivity.java
+++ app/src/main/java/androidlabs/com/androidlabs/LoginActivity.java
@@ -1,13 +1,72 @@
 package androidlabs.com.androidlabs;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
-
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+    private EditText edtEmail,edtPassword;
+    private Button btnLogin;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        sharedPreferences = getSharedPreferences("login",MODE_PRIVATE);
+
+        String email = sharedPreferences.getString("DefaultEmail","");
+        edtEmail.setText(email);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editor = sharedPreferences.edit();
+                editor.putString("DefaultEmail",edtEmail.getText().toString());
+                editor.commit();
+            }
+        });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+
     }
 }
