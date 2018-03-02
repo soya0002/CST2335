77b95aea2e90890410d76e76c6fb331c6c8f00fa
soya0002 <badal_bad123>
2018-03-02 16:54:12 -0500

    Finished Lab 5

---
+++ app/src/main/java/androidlabs/com/androidlabs/ChatDatabaseHelper.java
@@ -0,0 +1,43 @@
+package androidlabs.com.androidlabs;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by badal on 2018-03-01.
+ */
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper{
+
+    static String DATABASE_NAME = "Messages.db";
+    static int VERSION_NUM = 3;
+    static final String KEY_ID = "_id", KEY_MESSAGE = "message";
+    public static final String TABLE_NAME = "tbl_message";
+
+    public ChatDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+        db.execSQL("create table " + TABLE_NAME + " ( " + KEY_ID + " integer primary key autoincrement, " + KEY_MESSAGE + " text )");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + i + " newVersion=" + i1);
+        db.execSQL("drop table if exists " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int i, int i1) {
+        Log.i("ChatDatabaseHelper", "Calling onDowngrade, oldVersion=" + i + " newVersion=" + i1);
+        db.execSQL("drop table if exists " + TABLE_NAME);
+        onCreate(db);
+    }
+
+}
--- app/src/main/java/androidlabs/com/androidlabs/ChatWindow.java
+++ app/src/main/java/androidlabs/com/androidlabs/ChatWindow.java
@@ -1,11 +1,16 @@
 package androidlabs.com.androidlabs;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +22,18 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import static androidlabs.com.androidlabs.ChatDatabaseHelper.KEY_ID;
+import static androidlabs.com.androidlabs.ChatDatabaseHelper.KEY_MESSAGE;
+import static androidlabs.com.androidlabs.ChatDatabaseHelper.TABLE_NAME;
+
 public class ChatWindow extends Activity {
     ListView listView;
     EditText edtMessage;
     Button btnSend;
     ArrayList<String> messages;
     ChatAdapter messageAdapter;
+    ChatDatabaseHelper object;
+    protected static final String ACTIVITY_NAME = "ChatWindow";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +42,26 @@ public class ChatWindow extends Activity {
         edtMessage = (EditText) findViewById(R.id.edtMessage);
         btnSend = (Button) findViewById(R.id.btnSend);
         messages = new ArrayList<>();
+
+        object = new ChatDatabaseHelper(this);
+        final SQLiteDatabase db = object.getWritableDatabase();
+        final Cursor cursor = db.rawQuery("select * from " + TABLE_NAME,null);
+        if(cursor.getCount() > 0){
+            //cursor.moveToFirst();
+            if(cursor.moveToFirst()) {
+                while (!cursor.isAfterLast()) {
+                    int index = cursor.getColumnIndex(KEY_MESSAGE);
+                    String msg = cursor.getString(index);
+                    Log.i(ACTIVITY_NAME, "SQL MESSAGE:" + msg);
+                    messages.add(msg);
+                    cursor.moveToNext();
+                }
+            }
+        }
+
+        Log.i(ACTIVITY_NAME, "Cursor's  column count =" + cursor.getColumnCount() );
+        Log.i(ACTIVITY_NAME, "Column 1 : " + cursor.getColumnIndex(KEY_ID) + " Column 2 : " + cursor.getColumnIndex(KEY_MESSAGE));
+
         messageAdapter=new ChatAdapter( this );
         listView.setAdapter (messageAdapter);
         btnSend.setOnClickListener(new View.OnClickListener() {
@@ -39,12 +70,13 @@ public class ChatWindow extends Activity {
                 if(edtMessage.getText().toString().length() > 0){
                     messages.add(edtMessage.getText().toString());
                     messageAdapter.notifyDataSetChanged();
+                    ContentValues values = new ContentValues();
+                    values.put(KEY_MESSAGE,edtMessage.getText().toString());
+                    db.insert(TABLE_NAME,null,values);
                     edtMessage.setText("");
                 }
             }
         });
-
-
 
     }
 
@@ -83,6 +115,17 @@ public class ChatWindow extends Activity {
 
         }
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        object.close();
+        super.onDestroy();
 
     }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        throw new SQLiteException("Can't downgrade database from version " +
+                oldVersion + " to " + newVersion);
+    }
+
 }
